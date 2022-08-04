39af794621de12cad975b8d5bbff7fa082b1575b
Jorge Mendoza <jorgemendoza2002@gmail.com>
2022-08-04 18:47:50 -0500

    clicking the tab key in the text area now inputs four spaces instead of moving to the next focusable element on the page

--- src/components/DocumentEdit/MarkdownTextArea/MarkdownTextArea.tsx
+++ src/components/DocumentEdit/MarkdownTextArea/MarkdownTextArea.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 interface MarkdownTextAreaProps {
   setMarkdownText: React.Dispatch<string>;
 }
@@ -13,6 +15,21 @@ const MarkdownTextArea = ({ setMarkdownText }: MarkdownTextAreaProps) => {
         <textarea
           onChange={(e) => setMarkdownText(e.target.value)}
           onBlur={(e) => setMarkdownText(e.target.value)}
+          onKeyDown={(e) => {
+            const textArea = e.target;
+            if (e.key === 'Tab') {
+              e.preventDefault();
+              const start = e.target.selectionStart;
+              const end = e.target.selectionEnd;
+
+              textArea.value =
+                textArea.value.substring(0, start) +
+                '\t' +
+                textArea.value.substring(end);
+
+              textArea.selectionStart = start + 1;
+            }
+          }}
         />
       </div>
     </section>
