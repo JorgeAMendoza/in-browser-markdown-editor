7cd4321d2c45cbb1be4d80f76545ef867ac88fd6
Jorge Mendoza <jorgemendoza2002@gmail.com>
2022-09-09 14:36:22 -0500

    wrap the application in the redux provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,77 +1,10 @@
-import { useState } from 'react';
-import Menu from './components/Menu/Menu';
-import TopBar from './components/TopBar/TopBar';
 import DocumentEdit from './components/DocumentEdit/DocumentEdit';
-import welcomeMarkdownText from './util/welcome-markdown';
 import { SavedDocument } from './types/saved-document';
 
 function App() {
-  const [documentMarkdown, setDocumentMarkdown] = useState(welcomeMarkdownText);
-  const [documentTitle, setDocumentTitle] = useState('welcome.md');
-  const [isNewDocument, setIsNewDocument] = useState(true);
-  const [lastSavedDocument, setLastSavedDocument] = useState('');
-
-  // possilby another use effect for saving document! local storage will be needed then
-
-  // const changeDocument = () => {
-  //   // this funciton is going to be used to change the method
-  // };
-
-  const saveDocument = () => {
-    const savedDocument = localStorage.getItem('savedDocuments');
-    if (isNewDocument) {
-      if (savedDocument === null)
-        localStorage.setItem(
-          'savedDocuments',
-          JSON.stringify({ [documentTitle]: documentMarkdown })
-        );
-      else {
-        const savedDocumentObject = JSON.parse(savedDocument) as SavedDocument;
-
-        if (savedDocumentObject[documentTitle]) {
-          console.log(
-            'Document already exist, need to bring up the prompt to notify the user to change or overwrite'
-          );
-          return;
-        } else {
-          savedDocumentObject[documentTitle] = documentMarkdown;
-          localStorage.setItem(
-            'savedDocuments',
-            JSON.stringify(savedDocumentObject)
-          );
-        }
-      }
-      setLastSavedDocument(documentTitle);
-      setIsNewDocument(false);
-    } else {
-      const savedDocumentObject = JSON.parse(
-        savedDocument as string
-      ) as SavedDocument;
-      if (documentTitle === lastSavedDocument)
-        savedDocumentObject[documentTitle] = documentMarkdown;
-      else {
-        delete savedDocumentObject[lastSavedDocument];
-        savedDocumentObject[documentTitle] = documentMarkdown;
-        setLastSavedDocument(documentTitle);
-      }
-      localStorage.setItem(
-        'savedDocuments',
-        JSON.stringify(savedDocumentObject)
-      );
-    }
-  };
   return (
     <div>
-      <TopBar
-        documentTitle={documentTitle}
-        setDocumentTitle={setDocumentTitle}
-        saveDocument={saveDocument}
-      />
-      <Menu />
-      <DocumentEdit
-        markdownText={documentMarkdown}
-        setMarkdownText={setDocumentMarkdown}
-      />
+      <h1>Hello World</h1>
     </div>
   );
 }
--- src/types/document-context.ts
+++ src/types/document-context.ts
@@ -2,6 +2,7 @@ interface Document {
   currentDocumentTitle: string;
   documentMarkdown: string;
   lastDocumentTitle: null | string;
+  isNewDocument: boolean;
 }
 
 type DocumentContext = Document | null;
